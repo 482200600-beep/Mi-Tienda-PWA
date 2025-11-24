0b56d72af431429ba0bd009f5da8d77f6f58b6c4
482200600-beep <482200600@alumnos.utzac.edu.mx>
2025-11-24 00:10:35 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-// Configuración para desarrollo y producción
-export const API_URL = 'https://mi-tienda-pwa.onrender.com';
 export const API_URL = 
   window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3001'  // Desarrollo local
     : 'https://mi-tienda-pwa.onrender.com';  // Producción
-
