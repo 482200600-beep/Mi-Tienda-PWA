cb78c7803ffcadcb7abee55206f88a72b81fabcf
482200600-beep <482200600@alumnos.utzac.edu.mx>
2025-11-25 00:11:34 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 // src/config.js
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'https://mi-tienda-pwa.onrender.com',
-  APP_NAME: 'Mi Tienda PWA',
-  VERSION: '1.0.0'
+  API_URL: process.env.REACT_APP_API_URL || 'https://mi-tienda-pwa.onrender.com'
 };
-
-export const { API_URL, APP_NAME, VERSION } = config;
 export default config;
