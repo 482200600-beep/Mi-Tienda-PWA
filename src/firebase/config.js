783b2558e771cfb64bf5381f5d4220ca89ad6dce
482200600-beep <482200600@alumnos.utzac.edu.mx>
2025-11-26 01:00:39 -0600

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// ✅ TUS CREDENCIALES REALES - CORREGIDAS
+const firebaseConfig = {
+  apiKey: "AIzaSyB4i0eBOLPF38fwsGNuxLWtzflG4YtSkVc",
+  authDomain: "e-commerce-pwa-con-react.firebaseapp.com",
+  projectId: "e-commerce-pwa-con-react",
+  storageBucket: "e-commerce-pwa-con-react.firebasestorage.app",
+  messagingSenderId: "862933082794",
+  appId: "1:862933082794:web:52ce6d034b0050113b26d3"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar Firestore (base de datos)
+export const db = getFirestore(app);
+export default app;
